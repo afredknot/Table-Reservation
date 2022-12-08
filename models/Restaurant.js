e95dfae4bb841f39af603dbae9747332e0309b6e
mfrabott <mfrabott14@gmail.com>
2022-12-08 14:05:13 -0500

    merge Restaurant Model

---
+++ models/Restaurant.js
@@ -0,0 +1,59 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Restaurant extends Model {}
+
+Retaurant.init(
+  {
+    restaurant_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    menu_link: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    hours: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    floorplan_filepath: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'restaurant',
+  }
+);
+
+module.exports = Restaurant;
