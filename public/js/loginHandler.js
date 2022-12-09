90160513554c907f4c5c8daef17e1b3cb11f8653
Nate <fritz.nathaniel97@gmail.com>
2022-12-09 16:10:11 -0500

    resolving merge conflict

--- public/js/loginHandler.js
+++ public/js/loginHandler.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
       });
 
       if (response.ok) {
-        document.location.replace('/');
+        return document.location.replace('/');
       };
     }
     catch(err) {
