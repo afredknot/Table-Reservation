73c3c3443d2a81d05a0d05c78a53fb4f7fa3b53a
Impp18 <tdfallara@gmail.com>
2022-12-09 10:07:14 -0500

    added acutal signup rotue

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,6 +13,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+//Signup Route
+router.get('/signup', async (req, res) => {
+  try {
+    res.render('signup', {
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // Login route
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
