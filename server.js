450471d561663e120da0a976e95a92b491728aee
nathanrusselldev <nathanrussell.it@gmail.com>
2022-12-09 12:12:38 -0500

    cookie cookie cookie

--- server.js
+++ server.js
@@ -2,28 +2,32 @@
 const path = require('path');
 const express = require('express');
 // Import express-session
-const session = require('express-session');
 const exphbs = require('express-handlebars');
-
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize
   })
 };
-
-
 
 app.use(session(sess));
 
