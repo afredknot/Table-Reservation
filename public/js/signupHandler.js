68325f32fce6987af3af2a08cbc24b835103e3ea
Nate <fritz.nathaniel97@gmail.com>
2022-12-08 19:14:14 -0500

    Adds user creation handlebars file and js file for handling submit of that form

---
+++ public/js/signupHandler.js
@@ -0,0 +1,44 @@
+const signupForm = document.querySelector("#submitForm");
+const fName = document.querySelector("#firstName");
+const lName = document.querySelector("#lastName");
+const email = document.querySelector("#email");
+const phoneNum = document.querySelector("#phoneNum");
+const passOne = document.querySelector("#passOne");
+const passTwo = document.querySelector("#passTwo");
+const errorEl = document.querySelector("#errorText");
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    const data = {
+        firstName: fName.value.trim(),
+        lastName: lName.value.trim(),
+        email: email.value.trim().toLowerCase(),
+        phoneNumber: phoneNum.value,
+        password: ""
+    };
+
+    if(passOne.value == passTwo.value) {
+        body.password = passOne.value;
+    } else {
+        errorEl.innerHTML = "Error: Your passwords must match!";
+        passOne.value = "";
+        passTwo.value = "";
+        passOne.focus();
+    };
+
+    fetch("/api/users/new", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {"Content-Type": 'application/json'}
+    })
+    .then((response) => {
+        if(response.status == 200) {
+            alert("Account Creation Successful.");
+        } else {
+            errorEl.innerHTML = `Error: Code ${response.status}.`
+        };
+    });
+}
+
+signupForm.on("submit", handleSubmit)
