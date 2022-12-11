9e6e35bfbbb74390ea4a61f00a75b114ca437c8b
Daniel Nelson <afredknot@gmail.com>
2022-12-11 12:52:30 -0500

    reverting some of the handlebars back to previous to work again

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Reservation, Restaurant, DiningTable } = require('../../models');
+const { User, Reservation, Restaurant, DiningTable  } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
     const dbUserData = await User.create({
       first_name: req.body.first_name,
       last_name: req.body.last_name,  
-      phone: req.body.phone,
+      phone_number: req.body.phone_number,
       email: req.body.email,
       password: req.body.password,
     });
@@ -42,20 +42,19 @@ router.post('/login', async (req, res) => {
 
     const validPassword = await dbUserData.checkPassword(req.body.password);
 
-    // if (!validPassword) {
-    //   res
-    //     .status(400)
-    //     .json({ message: 'Incorrect email or password. Please try again!' });
-    //   return;
-    // }
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
 
     req.session.save(() => {
       req.session.loggedIn = true;
-      req.session.user = dbUserData.user_id;
 
       res
         .status(200)
-        .json({ user: dbUserData, message: `${dbUserData.first_name}, you are now logged in!` });
+        .json({ user: dbUserData, message: 'You are now logged in!' });
     });
   } catch (err) {
     console.log(err);
@@ -74,27 +73,20 @@ router.post('/logout', (req, res) => {
   }
 });
 
-//logout fetch 
-
-
-
-router.get('/reservations/:user_id', async (req, res) => {
+//Get one user
+router.get('/:id', async (req, res) => {
   try {
-
-    const reservationData = await Reservation.findAll(
-      {
-        where:{user_id: req.params.user_id}, 
-        include: [ 
-          {model: User, attributes: ['first_name', 'last_name']},
-          {model: Restaurant, attributes: ['name']},
-          {model: DiningTable, attributes: ['restaurant_table_ref']},
-        ]
-      }
-    );    
-  
-    const reservation = reservationData.map((reservation) => reservation.get({ plain: true }));
-
-    res.status(200).json(reservation);
+    const dbUserData = await User.findOne({
+      where: {
+      //use email to find user
+        email: req.body.email,
+      },
+    });
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user with this id' });
+      return;
+    }
+    res.status(200).json(dbUserData);
   } catch (err) {
     res.status(500).json(err);
   }
